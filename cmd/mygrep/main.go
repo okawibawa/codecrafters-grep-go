0cecb0f1959aa9664aac25d5617493b3f62a5d26
Oka Wibawa <okaa.wibawa@gmail.com>
2024-08-20 11:59:09 +0800

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -37,7 +37,7 @@ func main() {
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
+	if utf8.RuneCountInString(pattern) == 0 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
@@ -47,7 +47,11 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
+	if pattern == "\\d" {
+		ok = bytes.ContainsAny(line, "1234567890")
+	} else {
+		ok = bytes.ContainsAny(line, pattern)
+	}
 
 	return ok, nil
 }
